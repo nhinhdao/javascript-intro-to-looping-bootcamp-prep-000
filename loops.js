7dac4f3c280144b9becf333f37d4ac1b5b83a605
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-04-12 23:36:12 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(array){
+   for (var i = 1; i <= 25; i++){
+      if (i === 1) {array.push("I am ${i} strange loop.")};
+      else { array.push("I am ${i} strange loops.")};
+   }
+   return array;
+}
+
+function whileLoop(num){
+   while(num >= 0){
+      console.log(--num);
+   }
+   return `done`;
+}
+
+function maybeTrue(){
+   return Math.random() >= 0.5;
+}
+
+function doWhileLoop(array){
+   do{
+      array.shift();
+   }while (array.length > 0 && maybeTrue());
+}
+
+var array = ["I am a strange loop"];
+var array2 = ["I am a strange loop","I am 1 strange loop","I am 2 strange loop","I am 3 strange loop","I am 4 strange loop","I am 5 strange loop"];
+console.log(forLoop(array));
+console.log(whileLoop(10));
+console.log(doWhileLoop(array2));
